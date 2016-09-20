e1e0ba249015411fccdd4d9fcb4b082b0e196fd7
Ryan Cobb <ryan.cobb@mx.com>
2016-09-20 10:51:26 -0600

    add rocket to list of non-workable emojis

--- crushit.js
+++ crushit.js
@@ -58,7 +58,7 @@ function findQueue(queue) {
 }
 
 function isWorkable(item) {
-  var nonWorkableEmojis = [ "punch.png", "shipit.png", "interrobang.png", "bangbang.png", "hourglass.png", "muscle.png" ];
+  var nonWorkableEmojis = [ "rocket.png", "punch.png", "shipit.png", "interrobang.png", "bangbang.png", "hourglass.png", "muscle.png" ];
   var workable = true;
 
   nonWorkableEmojis.forEach(function(emoji) {
